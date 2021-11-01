256fb7a12df02d5cc3bca87a94d246030630a5f3
Hambush2 <55706777+Hambush2@users.noreply.github.com>
2021-11-01 22:50:37 +0000

    Added Player and Camera Movement
    Added the ability for the player to move around a space using WASD and the Space Bar (for jump).
    
    Added the ability to look around using the mouse. Press escape to access the cursor.

---
+++ Temple Raider/Assets/Scripts/CharacterController.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CharacterController : MonoBehaviour
+{
+    public float speed = 10;
+    public float jump = 10;
+    Rigidbody rbody;
+    CapsuleCollider collider;
+    // Start is called before the first frame update
+    void Start()
+    {
+        Cursor.lockState = CursorLockMode.Locked;
+        rbody = GetComponent<Rigidbody>();
+        collider = GetComponent<CapsuleCollider>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        float fwdBck = Input.GetAxis("Vertical") * speed;
+        float straffe = Input.GetAxis("Horizontal") * speed;
+        fwdBck *= Time.deltaTime;
+        straffe *= Time.deltaTime;
+
+        transform.Translate(straffe, 0, fwdBck);
+
+        if (Input.GetKeyDown("escape"))
+        {
+            Cursor.lockState = CursorLockMode.None;
+        }
+
+        if (Input.GetKeyDown("space") && IsGrounded())
+        {
+            rbody.AddForce(new Vector3(0, jump, 0), ForceMode.Impulse);
+        }
+    }
+
+    bool IsGrounded()
+    {
+        float distToGround = collider.bounds.extents.y;
+        return Physics.Raycast(transform.position, -Vector3.up, distToGround + 0.1f);
+    }
+}
