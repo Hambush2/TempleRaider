2f4e739f140a9e415dd2a5433161f60731caec07
Hambush2 <55706777+Hambush2@users.noreply.github.com>
2022-01-10 18:41:47 +0000

    Created Initial outline for basic trap tile
    Created the basic tile for the basic corridor tile, and added a health system scrip

---
+++ Temple Raider/Assets/Scripts/HealthSystem.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Events;
+
+[System.Serializable]
+public class OnDamagedEvent : UnityEvent<int> { }
+public class HealthSystem : MonoBehaviour
+{
+    public int health = 10;
+    public UnityEvent onDie;
+    public OnDamagedEvent onDamaged;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+    public void TakeDamage(int damage)
+    {
+        health -= damage;
+
+        onDamaged.Invoke(health);
+
+        if (health < 1)
+        {
+            onDie.Invoke();
+        }
+    }
+}
